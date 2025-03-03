4fa0be4703924b488c92a46f9228b1dc063e2efc
Tom Dymel <tom@dymel.dev>
2025-03-03 23:13:39 +0100

    Initial work to translate the page, todo: news section

---
+++ src/i18n.ts
@@ -0,0 +1,158 @@
+export const languages = {
+    de: "Deutsch",
+    en: "English",
+    ru: "Русский",
+};
+
+export const supportedLanguages = Object.keys(languages);
+
+export const defaultLang = "de";
+
+export const translations = {
+    de: {
+        index: {
+            header: {
+                greeting: "Moin",
+                weAre: "Wir sind Sparkle Squad!",
+                subTitle: "Mixed Volleyball-Team um Hamburg Hamm",
+                text: "Du suchst nach einem ambitionierten und sympathischen Team und du hast sichere Grundlagen? Dann bist du bei uns genau richtig! Komm zum Probetraining und mach dir ein Bild von uns!",
+                comeToTrial: "Komm zum Probetraining!",
+                followOnInstagram: "Folge uns auf Instagram!"
+            },
+            qualities: {
+                title: "Das zeichnet uns aus",
+                team: {
+                    title: "Ambitioniertes, sympathisches und multi-Linguales Team",
+                    desc: "Wir wollen uns alle stetig verbessern und wir geben immer 110%. Wir halten uns den Rücken frei und feuern uns gegenseitig an. Wir sprechen Deutsch, Englisch, Ukrainisch und Russisch."
+                },
+                training: {
+                    title: "Strukturiertes und zielgerichtetes Training",
+                    desc: "Wir machen ein zielgerichtetes Training, um uns stetig zu verbessern. Zuerst machen wir uns gemeinsam warm und machen etwas Krafttraining. Danach verbessern wir unsere Grundtechnik oder üben Spielszenarien. Am Ende spielen wir und wenden das gelernte an."
+                },
+                tournaments: {
+                    title: "Teilnahme an Turnieren",
+                    desc: "Wir spielen nicht nur in der Mixed-Runde (A4) und dem Hansa-Pokal mit, sondern auch an zahlreichen anderen Hallen und Beach-Turnieren im Umkreis von Hamburg."
+                },
+                outside: {
+                    title: "Im Sommer gehen wir raus",
+                    desc: "Im Sommer gehts raus an die frische Luft. Wir treffen uns dann im Stadtpark, um dort auf dem Sand oder Rasen zu spielen."
+                }
+            },
+            newsTitle: "Funkelnde Neuigkeiten"
+        },
+        contact: {
+            title: "Werde teil des Sparkle Squad's!",
+            text1: "Im Team sprechen wir hauptsächlich Deutsch, aber wir können auch auf Englisch, Russisch und Ukrainisch weiterhelfen.",
+            writeUs: "Schreib uns!"
+        },
+        fourofour: {
+            title: "Leider konnte die Seite nicht gefunden werden.",
+            backToHome: "Zurück zur Übersicht"
+        },
+        sidebar: {
+            news: "Neuigkeiten",
+            contact: "Kontakt"
+        }
+    },
+    en: {
+        index: {
+            header: {
+                greeting: "Hello",
+                weAre: "We are Sparkle Squad!",
+                subTitle: "Mixed Volleyball Team around Hamburg Hamm",
+                text: "Are you looking for an ambitious and friendly team with solid fundamentals? Then you are exactly right with us! Come to a trial and see what we are all about!",
+                comeToTrial: "Come to a trial!",
+                followOnInstagram: "Follow us on Instagram!"
+            },
+            qualities: {
+                title: "What sets us apart",
+                team: {
+                    title: "Ambitious, friendly, and multilingual team",
+                    desc: "We all strive to continually improve and always give 110%. We have each other's backs and cheer each other on. We speak German, English, Ukrainian, and Russian."
+                },
+                training: {
+                    title: "Structured and goal-oriented training",
+                    desc: "We conduct focused training to continuously improve. We start by warming up together and doing some strength training. Then we improve our basic technique or practice game scenarios. At the end, we play and apply what we have learned."
+                },
+                tournaments: {
+                    title: "Participation in tournaments",
+                    desc: "We not only participate in the Mixed Round (A4) and the Hansa Cup but also in numerous other indoor and beach tournaments around Hamburg."
+                },
+                outside: {
+                    title: "We go outside in the summer",
+                    desc: "In the summer, we head out into the fresh air. We meet in the city park to play on the sand or grass."
+                }
+            },
+            newsTitle: "Sparkling News"
+        },
+        contact: {
+            title: "Become part of the Sparkle Squad!",
+            text1: "In the team, we mainly speak German, but we can also assist in English, Russian, and Ukrainian.",
+            writeUs: "Write to us!"
+        },
+        fourofour: {
+            title: "Unfortunately, the page could not be found.",
+            backToHome: "Back to Overview"
+        },
+        sidebar: {
+            news: "News",
+            contact: "Contact"
+        }
+    },
+    ru: {
+        index: {
+            header: {
+                greeting: "Привет",
+                weAre: "Мы - Sparkle Squad!",
+                subTitle: "Смешанная волейбольная команда в районе Гамбург Хамм",
+                text: "Вы ищете амбициозную и дружелюбную команду с прочной базой? Тогда вам точно к нам! Приходите на пробное занятие и посмотрите, что мы из себя представляем!",
+                comeToTrial: "Приходите на пробное занятие!",
+                followOnInstagram: "Подписывайтесь на нас в Instagram!"
+            },
+            qualities: {
+                title: "Что нас отличает",
+                team: {
+                    title: "Амбициозная, дружелюбная и многоязычная команда",
+                    desc: "Мы все стремимся к постоянному улучшению и всегда выкладываемся на 110%. Мы поддерживаем и подбадриваем друг друга. Мы говорим на немецком, английском, украинском и русском языках."
+                },
+                training: {
+                    title: "Структурированная и целенаправленная тренировка",
+                    desc: "Мы проводим целенаправленные тренировки для постоянного совершенствования. Сначала разогреваемся вместе и выполняем силовые упражнения, улучшаем базовую технику или отрабатываем игровые сценарии. В конце мы играем и применяем полученные навыки."
+                },
+                tournaments: {
+                    title: "Участие в турнирах",
+                    desc: "Мы участвуем не только в Mixed-Runde (A4) и Hansa-Pokal, в том числе и пляжных турнирах в окрестностях Гамбурга."
+                },
+                outside: {
+                    title: "Летом мы выходим в парк",
+                    desc: "Летом мы выходим на свежий воздух. Мы встречаемся в городском парке, чтобы поиграть на песке или траве."
+                }
+            },
+            newsTitle: "Sparkle Squad новости"
+        },
+        contact: {
+            title: "Станьте частью Sparkle Squad!",
+            text1: "В команде мы в основном говорим на немецком, но можем помочь и на английском, русском и украинском языках.",
+            writeUs: "Напишите нам!"
+        },
+        fourofour: {
+            title: "К сожалению, страница не найдена.",
+            backToHome: "Вернуться к обзору"
+        },
+        sidebar: {
+            news: "Новости",
+            contact: "Контакт"
+        }
+    },
+} as const;
+
+export function getLangFromUrl(url: URL) {
+    const [, lang] = url.pathname.split('/');
+    if (lang in translations) return lang as keyof typeof translations;
+    return defaultLang;
+}
+
+export function useTranslations(url: URL) {
+    const lang = getLangFromUrl(url);
+    return translations[lang];
+}
