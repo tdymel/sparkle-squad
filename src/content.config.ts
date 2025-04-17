f9c199c2123e8cd2da03aebb152e5eb3e9e9c82c
Tom Dymel <tom@dymel.dev>
2025-04-17 16:17:20 +0200

    Update deps

---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+
+const blogSchema = z.object({
+    identifier: z.string(),
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.string().optional(),
+    heroImage: z.string().optional(),
+    badge: z.string().optional(),
+});
+
+const blogCollection = defineCollection({ schema: blogSchema });
+
+export const collections = {
+    news: blogCollection,
+};
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,1 @@
-import { z, defineCollection } from "astro:content";
-const blogSchema = z.object({
-    identifier: z.string(),
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.string().optional(),
-    heroImage: z.string().optional(),
-    badge: z.string().optional(),
-});
-
-export type BlogSchema = z.infer<typeof blogSchema>;
-
-const blogCollection = defineCollection({ schema: blogSchema });
-
-export const collections = {
-    'news': blogCollection,
-}
+import { defineCollection, z } from "astro:content";
